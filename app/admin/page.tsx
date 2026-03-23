6879a372cb7131677fecb3f398a45633a1894f32
Ulfath <ulfathwaliullah@gmail.com>
2026-03-23 17:16:05 +0100

    Create admin page and move code to this page

---
+++ app/admin/page.tsx
@@ -0,0 +1,56 @@
+import type { Category, ProductsResponse } from "../types";
+import Header from "../components/header";
+import Footer from "../components/Footer-component";
+import ProductsTable from "../components/ProductsTable";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000";
+const DEFAULT_LIMIT = 20;
+
+export default async function Admin({
+  searchParams,
+}: Readonly<{
+  searchParams: Promise<{ page?: string; search?: string }>;
+}>) {
+  const params = await searchParams;
+
+  const currentPage = Math.max(1, Number(params.page) || 1);
+  const search = params.search || "";
+
+  const [allProductsResponse, pagedProductsResponse, categories] =
+    await Promise.all([
+      fetch(`${API_URL}/products/?_sort=id&_order=desc&_expand=category`).then(
+        (res) => res.json(),
+      ) as Promise<ProductsResponse>,
+      fetch(
+        `${API_URL}/products/?_limit=${DEFAULT_LIMIT}&_sort=id&_order=desc&_expand=category&_page=${currentPage}&title_like=${search}`,
+      ).then((res) => res.json()) as Promise<ProductsResponse>,
+      fetch(`${API_URL}/categories`).then((res) => res.json()) as Promise<
+        Category[]
+      >,
+    ]);
+
+  const { products: allProducts, total: totalProducts } = allProductsResponse;
+  const { products, total, pages } = pagedProductsResponse;
+
+  return (
+    <>
+      <Header
+        products={allProducts}
+        total={totalProducts}
+        categories={categories}
+      />
+      <main className="w-full pl-70 pt-70 pb-15 bg-gray-50">
+        <div>
+          <ProductsTable products={products} categories={categories} />
+        </div>
+      </main>
+      <Footer
+        total={total}
+        pages={pages}
+        numberOfProducts={products.length}
+        currentPage={currentPage}
+        limit={DEFAULT_LIMIT}
+      />
+    </>
+  );
+}
