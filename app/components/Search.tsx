51d4527ff31d014957e7b5490d4058209c695334
Ulfath <ulfathwaliullah@gmail.com>
2026-03-23 17:18:49 +0100

    Fix issue with search where it would redirect the website.

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,39 +1,40 @@
-'use client';
+"use client";
 
-import { Search as SearchIcon } from 'lucide-react';
-import { useRouter } from 'next/navigation';
-import { useState, useEffect } from 'react';
+import { Search as SearchIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
 export default function Search() {
   const router = useRouter();
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
   useEffect(() => {
+    if (!value) return;
     const delayDebounceFn = setTimeout(() => {
       // Update the URL with the search query
-      router.push(`/?search=${value}`);
+      router.push(`${window.location.pathname}?search=${value}`);
     }, 500); // Adjust the debounce delay as needed
     return () => clearTimeout(delayDebounceFn);
   }, [value, router]);
 
   return (
-    <div className='relative w-full max-w-md'>
+    <div className="relative w-full max-w-md">
       {/* Icon */}
       <SearchIcon
-        className='absolute left-3 top-1/2 -translate-y-1/2 text-gray-500'
+        className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
         size={16}
       />
 
-      <label htmlFor='search-input' className='sr-only'>
+      <label htmlFor="search-input" className="sr-only">
         Search products
       </label>
       <input
-        className='w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-purple-600'
-        type='search'
-        name='search-input'
+        className="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-400 focus:border-purple-600"
+        type="search"
+        name="search-input"
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        placeholder='Search products...'
+        placeholder="Search products..."
       />
     </div>
   );
