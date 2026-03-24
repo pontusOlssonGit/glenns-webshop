f34b1d3dbb1ffad5f3453c9cc989a2d19fc55bfb
pontusOlssonGit <pontusOlssonEmail@gmail.com>
2026-03-24 21:14:24 +0100

    categories header

--- components/ClientSideHeader.tsx
+++ components/ClientSideHeader.tsx
@@ -2,6 +2,15 @@ import { CircleDashed } from "lucide-react";
 import MainNavigation from "./MainNavigation";
 
 export default function Header() {
+
+  const categories = [
+    "Tanks",
+    "Sprängämnen",
+    "Atombomber",
+    "Scudmissiler",
+    "Stridsflyg",
+    "Minröjare",
+  ];
   return (
     <header className="bg-[#1a1a1d] w-full">
       <div className="pl-40 pr-40 flex justify-between border-b-4 border-blue-500">
@@ -15,26 +24,13 @@ export default function Header() {
 
       <div className="w-full bg-gray-800 flex pl-40 pr-40">
           <ul className="flex flex-row w-full justify-around text-center text-white text-xs font-semibold">
-            <li className="w-full h-full hover:bg-[#1a1a1d] p-4 cursor-pointer">
-              Tanks
-            </li>
-            <li className="w-full h-full hover:bg-[#1a1a1d] p-4 cursor-pointer">
-              Sprängämnen
-            </li>
-            <li className="w-full h-full hover:bg-[#1a1a1d] p-4 cursor-pointer">
-              Atombomber
-            </li>
-            <li className="w-full h-full hover:bg-[#1a1a1d]  p-4 cursor-pointer">
-              Scudmissiler
-            </li>
-            <li className="w-full h-full hover:bg-[#1a1a1d]  p-4 cursor-pointer">
-              Stridsflyg
-            </li>
-            <li className="w-full h-full hover:bg-[#1a1a1d]  p-4 cursor-pointer">
-              Minröjare
-            </li>
+            {categories.map((category) => (
+              <li key={category} className="w-full h-full hover:bg-[#1a1a1d] p-4 cursor-pointer">
+                {category}
+              </li>
+            ))}
           </ul>
-        </div>
+      </div>
 
     </header>
   );
