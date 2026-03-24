31a5ab979adc1333de47cb09509deb5b49186124
pontusOlssonGit <pontusOlssonEmail@gmail.com>
2026-03-25 00:08:40 +0100

    -

--- components/ClientSideHeader.tsx
+++ components/ClientSideHeader.tsx
@@ -18,9 +18,9 @@ export default function Header() {
   return (
     <header className="bg-[#1a1a1d] w-full">
       <div className="pl-40 pr-40 flex justify-between border-b-4 border-[#3338ff]">
-        <div className="flex pl-7 items-center justify-center font-bold text-2xl text-white relative">
+        <div className="flex items-center justify-center font-bold text-2xl text-white">
           <Link href="/" className="flex items-center gap-2">
-          <CircleDashed className="w-10 h-10 text-gray-500 absolute -left-5 animate-spin [animation-duration:30s]" />
+          <CircleDashed className="w-10 h-10 text-gray-500 animate-spin [animation-duration:30s]" />
           Glennhallen
           </Link>
         </div>
@@ -31,7 +31,7 @@ export default function Header() {
       <div className="w-full bg-[#333] flex pl-40 pr-40">
           <ul className="flex flex-row w-full justify-around text-center text-white text-xs font-semibold">
             {categories.map((category) => (
-              <li key={category} className="w-full h-full hover:bg-[#1a1a1d] p-4 cursor-pointer">
+              <li key={category} className="w-full hover:bg-[#1a1a1d] p-2 cursor-pointer">
                 {category}
               </li>
             ))}
