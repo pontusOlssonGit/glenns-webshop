90c7507a8fb241f46974630efe591b4db59f3aba
pontusOlssonGit <pontusOlssonEmail@gmail.com>
2026-03-24 20:55:01 +0100

    product page extension

--- components/product.tsx
+++ components/product.tsx
@@ -1,7 +1,7 @@
 import { Product } from "@/types/types";
 import Image from "next/image";
 import ProductRating from "./product-rating";
-import { Check, Square, X } from "lucide-react";
+import { Check, Square, User2, X } from "lucide-react";
 
 export default function ProductComponent({ product }: { product: Product }) {
   return (
@@ -11,7 +11,7 @@ export default function ProductComponent({ product }: { product: Product }) {
         {product.sku}
       </p>
       <ProductRating productRating={product.rating} />
-      <section className="flex flex-row justify-between items-center border-b">
+      <section className="flex flex-row justify-between items-center border-b border-gray-300">
         <Image
           src={product.images[0]}
           alt={product.title}
@@ -19,7 +19,7 @@ export default function ProductComponent({ product }: { product: Product }) {
           height={500}
         />
         <div className="w-full flex flex-col items-center justify-center gap-4">
-          <h2 className="text-3xl font-extrabold">
+          <h2 className="text-6xl font-extrabold">
             {Math.ceil(product.price)} kr
           </h2>
           <span className="text-sm text-gray-500">(Excluding Glenn Tax)</span>
@@ -27,7 +27,8 @@ export default function ProductComponent({ product }: { product: Product }) {
             type="button"
             className="px-6 py-3 w-full rounded-full bg-[#3338ff] text-white mt-6 hover:bg-[#1e21ff] transition-colors"
           >
-            Add to cart
+            <span className="font-semibold">Lägg i varukorg</span>
+         
           </button>
           <span className="font-semibold">Gratis frakt vid köp över 500 000 kr</span>
           
@@ -45,23 +46,50 @@ export default function ProductComponent({ product }: { product: Product }) {
           </span>
           </div>
 
+          <div className="bg-gray-50 w-full rounded-lg p-2 text-left">
+          <h2 className="font-extrabold">Tekniska Specifikationer</h2>
+          <div className="text-sm flex gap-1 items-center">
+            <h3 className="font-semibold">Djup:</h3>
+            <span>{product.dimensions?.depth} m</span>
+            
+            
+          </div>
+          <div className="text-sm flex gap-1 items-center">
+            <h3 className="font-semibold">Höjd:</h3>
+            <span>{product.dimensions?.height} m</span>
+            
+            
+          </div>
+          <div className="text-sm flex gap-1 items-center">
+            <h3 className="font-semibold">Bredd:</h3>
+            <span>{product.dimensions?.width} m</span>
+            
+          </div>
+          </div>
+
         </div>
       </section>
 
       <section>
         <div className="flex flex-col gap-4">
             <div>
-                <h2 className="font-semibold">Översikt</h2>
+                <h2 className="font-extrabold">Översikt</h2>
                 <p>{product.description}</p>
             </div>
                <div>
-                <h2 className="font-semibold">Recensioner{product.reviews ? ` (${product.reviews.length})` : ""}</h2>
-                <ul>
+                <h2 className="font-extrabold">Recensioner{product.reviews ? ` (${product.reviews.length})` : ""}</h2>
+                <ul className="flex flex-col gap-4">
                     {product.reviews?.map((review, index) => (
-                        <li key={index}>
-                            {review.reviewerEmail}
-                            {review.reviewerName}
-                            {review.rating}
+                        <li key={index} className="flex flex-col items-start gap-2 pt-4 pb-4 border-b border-gray-300">
+                            <div className="flex flex-row items-center gap-2">
+                                <span className="bg-[#0004ff] p-3 rounded-full"><User2 className="w-6 h-6 text-white" /></span>
+                                <div>
+                                    <span>{review.reviewerName}</span>
+                                    <ProductRating productRating={review.rating} />
+                                </div>
+                                
+                            </div>
+                            
                             {review.comment}
                         </li>
                     ))}
