3a8311bc0d91ceac9a397e0508a6d0ce8a7452c4
Ulfath <ulfathwaliullah@gmail.com>
2026-03-24 13:59:43 +0100

    Create basic header

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <header className="bg-[#088395] h-15 w-full">
+        <div className="max-w-6xl !mx-auto h-full"></div>
+      </header>
+    </div>
+  );
 }
