f4fcb08e81ea3d4207fc3725c3f8e79aa7496659
Ulfath <ulfathwaliullah@gmail.com>
2026-03-24 14:14:02 +0100

    Move nav to it's own component

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,12 @@
-"use client";
+import MainNavigation from "@/components/MainNavigation";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-const menu = [
-  { title: "Home", href: "/" },
-  { title: "Cart", href: "/" },
-  { title: "About", href: "/" },
-];
 
 export default function Home() {
   return (
     <div>
       <header className="bg-[#088395] h-15 w-full">
-        <div className="max-w-6xl !mx-auto h-full">
-          <nav>
-            <ul className="w flex justify-around">
-              {menu.map((item, index) => (
-                <li key={index}>
-                  <Link
-                    className=" flex h-15 w-[83px] items-center justify-center  hover:bg-[#09637E] text-black"
-                    href={item.href}
-                  >
-                    {item.title}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
+        <div className="max-w-6xl mx-auto! h-full">
+          <MainNavigation />
         </div>
       </header>
     </div>
