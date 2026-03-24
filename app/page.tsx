62b689a11ac84ffaa1126a5c594bc1af856a910e
Ulfath <ulfathwaliullah@gmail.com>
2026-03-24 14:03:58 +0100

    Create basic nav for temprorary links

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+const menu = [
+  { title: "Home", href: "/" },
+  { title: "Cart", href: "/" },
+  { title: "About", href: "/" },
+];
 
 export default function Home() {
   return (
     <div>
       <header className="bg-[#088395] h-15 w-full">
-        <div className="max-w-6xl !mx-auto h-full"></div>
+        <div className="max-w-6xl !mx-auto h-full">
+          <nav>
+            <ul className="w flex justify-around">
+              {menu.map((item, index) => (
+                <li key={index}>
+                  <Link
+                    className=" flex h-15 w-[83px] items-center justify-center  hover:bg-[#09637E] text-black"
+                    href={item.href}
+                  >
+                    {item.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
       </header>
     </div>
   );
