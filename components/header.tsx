294b86ce15f0efe64bea258c58d271b509a40058
Ulfath <ulfathwaliullah@gmail.com>
2026-03-24 15:35:09 +0100

    Fix imports in some components

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,9 @@
-import { Funnel, Plus } from 'lucide-react';
-import StatCard from './HeaderStatCard';
-import { Category, Product } from '../types';
-import AddProductButton from './ProductAddButton';
-import Search from './Search';
+import { Funnel, Plus } from "lucide-react";
+import StatCard from "./HeaderStatCard";
+
+import AddProductButton from "./ProductAddButton";
+import Search from "./Search";
+import { Category, Product } from "@/types/types";
 
 interface HeaderProps {
   products: Product[];
@@ -18,46 +19,46 @@ export default function Header({
   console.log(categories);
   return (
     <>
-      <header className='fixed left-64 top-0 right-0 z-30'>
-        <section className='px-8 py-4 bg-white flex items-center justify-between'>
+      <header className="fixed left-64 top-0 right-0 z-30">
+        <section className="px-8 py-4 bg-white flex items-center justify-between">
           <div>
-            <h1 className='text-xl font-bold'>Product management</h1>
-            <span className='text-gray-500 text-sm'>
+            <h1 className="text-xl font-bold">Product management</h1>
+            <span className="text-gray-500 text-sm">
               Manage your store inventory
             </span>
           </div>
           <AddProductButton />
         </section>
         <StatCard products={allProducts} total={totalProducts} />
-        <div className='flex items-center text-xs gap-4 mb-6 border-gray-300 px-8 py-4 bg-white'>
+        <div className="flex items-center text-xs gap-4 mb-6 border-gray-300 px-8 py-4 bg-white">
           <Search />
-          <select className='px-3 py-2 border border-gray-300 rounded-md focus:outline-	none focus:ring-2 focus:ring-purple-600'>
-            <option value=''>All Categories</option>
+          <select className="px-3 py-2 border border-gray-300 rounded-md focus:outline-	none focus:ring-2 focus:ring-purple-600">
+            <option value="">All Categories</option>
             {categories?.map((category) => (
               <option key={category.id} value={category.id}>
                 {category.name}
               </option>
             ))}
           </select>
-          <label htmlFor='status' className='sr-only'>
+          <label htmlFor="status" className="sr-only">
             Stock status
           </label>
           <select
-            id='status'
-            className='px-3 py-2 border border-gray-300 rounded-md focus:outline-	none focus:ring-2 focus:ring-purple-600'
+            id="status"
+            className="px-3 py-2 border border-gray-300 rounded-md focus:outline-	none focus:ring-2 focus:ring-purple-600"
           >
-            <option value=''>All Stock Status</option>
-            <option value='1'>Low</option>
-            <option value='2'>Medium</option>
-            <option value='3'>High</option>
+            <option value="">All Stock Status</option>
+            <option value="1">Low</option>
+            <option value="2">Medium</option>
+            <option value="3">High</option>
           </select>
           {/* <div className='flex items-center px-3 py-2 border border-gray-300 rounded-md'>
             <Funnel className='w-4 h-4' />
             <button type='button'>Filter</button>
           </div> */}
-          <div className='flex gap-2 items-center px-3 py-2 border border-gray-300 rounded-md'>
-            <Funnel className='w-4 h-4 text-gray-600' />
-            <button type='button' className='text-sm font-medium'>
+          <div className="flex gap-2 items-center px-3 py-2 border border-gray-300 rounded-md">
+            <Funnel className="w-4 h-4 text-gray-600" />
+            <button type="button" className="text-sm font-medium">
               Filter
             </button>
           </div>
