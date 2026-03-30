0a474e8b1dbf036c6babd6d5002fd464c0b7ec4d
Fredrik.N <fredrik.nelin@outlook.com>
2026-03-30 14:37:08 +0200

    Last changes for today

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -13,8 +13,10 @@ export default function ContactForm() {
             <form className="grid">
                 <label htmlFor="name">Namn:</label>
                 <input type="text" id="name" name="name" placeholder="Kalle Kula" required />
+
                 <label htmlFor="email">Epost:</label>
                 <input type="email" id="email" name="email" placeholder="kalle.kula@exempel.se" required />
+
                 <label htmlFor="subject">Ämne:</label>
                 <select id="subject" name="subject" defaultValue="startvarde" required>
                     <option value="startvarde" disabled>Välj ett alternativ...</option>
@@ -23,12 +25,15 @@ export default function ContactForm() {
                     <option value="betalning">Betalning/Faktura</option>
                     <option value="ovrigt">Övrigt</option>
                 </select>
+
                 <label htmlFor="message">Meddelande:</label>
                 <textarea id="message" name="message" placeholder="Ditt meddelande..." required></textarea>
+
                 <section className="flex gap-4">
                     <button type="submit">Skicka</button>
                     <button type="button" onClick={RensBaraOmSakerDialog}>Ångra</button>
                 </section>
+
             </form>
         </>
     );
