6f6c7568c1c942a09da7132304f1e1366a99d76e
pontusOlssonGit <pontusOlssonEmail@gmail.com>
2026-04-02 11:36:08 +0200

    fixed issue with not getting all data from DB

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,7 @@
 import ProductComponent from "@/components/product";
 import { createClient } from "@/lib/supabase/server";
 import { ProductsResponse } from "@/types/types";
+import { log } from "console";
 import { notFound } from "next/navigation";
 
 export default async function ProductPage({ params}: { params: Promise<{ id: number }>}) {
@@ -18,6 +19,44 @@ export default async function ProductPage({ params}: { params: Promise<{ id: num
   if (error || !product) {
     notFound();
   }
+
+  const { data: images, error: imageError } = await supabase
+    .from('images')
+    .select('url')
+    .eq('product_id', paramsId)
+    
+
+  if (imageError) {
+    console.log("Error fetching image:", imageError); 
+  }
+
+  const { data: reviews, error: reviewError } = await supabase
+    .from('reviews')
+    .select('*')
+    .eq('product_id', paramsId)
+    
+
+  if (reviewError) {
+    console.log("Error fetching reviews:", reviewError); 
+  }
+
+  const {data: dimensions, error: dimensionsError} = await supabase
+    .from('dimensions')
+    .select('*')
+    .eq('product_id', paramsId)
+    .single();
+
+  if (dimensionsError) {
+    console.log("Error fetching dimensions:", dimensionsError); 
+  }
+
+  product.dimensions = dimensions;
+  product.image = images?.[0]?.url;
+  product.reviews = reviews;
+  console.log(product);
+  
+  
+  
   
   return (
     <ProductComponent product={product} />
--- types/types.ts
+++ types/types.ts
@@ -43,6 +43,7 @@ export interface Product {
     qrCode?: string;
   };
   images: string[];
+  image?: string;
   thumbnail: string;
 }
 
