8586250d4139c23af81f4e346ebd6e238f81565c
pontusOlssonGit <pontusOlssonEmail@gmail.com>
2026-04-07 18:17:42 +0200

    fix #4 deploymeny

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -1,7 +1,7 @@
 'use server'
 
 import { createClient } from '@/lib/supabase/server'
-import { refresh, revalidatePath } from 'next/cache'
+import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 export async function login(formData: FormData) {
