08489bef8d8a0b09570f7a825c9b560e074b2ecd
Ulfath <ulfathwaliullah@gmail.com>
2026-04-08 21:04:16 +0200

    Add a little notification counter to the cart icon.

--- components/MainNavigation.tsx
+++ components/MainNavigation.tsx
@@ -3,49 +3,66 @@ import { ShoppingCartIcon, User2Icon } from "lucide-react";
 import Link from "next/link";
 
 import { signOut } from "@/lib/actions/auth";
-
-
-
-
-
-
+import { useCartStore } from "./Store";
 
 export default function MainNavigation({ user }: { user: any }) {
-  
-  
-  
-
   const menu = [
     { title: "Logga in", href: "/login" },
     { title: "Skapa konto", href: "/signup" },
   ];
+  const productsInCart = useCartStore((state) => state.cartItems);
+  const numberCartItems = productsInCart.reduce(
+    (acc, item) => acc + item.quantity,
+    0,
+  );
   return (
     <nav className="pt-3 pb-3">
       <ul className="w-full flex justify-around items-center gap-4">
-        <span><User2Icon className="text-white w-10 h-10" /></span>
+        <span>
+          <User2Icon className="text-white w-10 h-10" />
+        </span>
         <div className="flex flex-col items-center">
-        {!user ? (
-          menu.map((item, index) => (
-            <li key={index} className="hover:underline text-white">
-              <Link
-                href={item.href}
+          {!user ? (
+            menu.map((item, index) => (
+              <li key={index} className="hover:underline text-white">
+                <Link href={item.href}>
+                  <span className="font-semibold text-sm">{item.title}</span>
+                </Link>
+              </li>
+            ))
+          ) : (
+            <li className="flex flex-col items-center text-white">
+              <span className="text-xs truncate">{user?.email}</span>
+              <span
+                onClick={() => {
+                  signOut();
+                }}
+                className="cursor-pointer font-semibold text-sm hover:underline"
               >
-                <span className="font-semibold text-sm">{item.title}</span>
-              </Link>
+                Logga ut
+              </span>
             </li>
-          ))
-        ) : (
-          <li className="flex flex-col items-center text-white">
-            <span className="text-xs truncate">{user?.email}</span>
-              <span onClick={() => {signOut()}} className="cursor-pointer font-semibold text-sm hover:underline">Logga ut</span>
-          </li>
-        )}
-        
+          )}
         </div>
-        <Link href="/cart" className="flex items-center gap-1">
-          <li><ShoppingCartIcon className="text-gray-600 w-10 h-10" /></li>
+        <Link
+          href="/cart"
+          className="flex items-center gap-1 relative inline-flex items-center p-3"
+        >
+          <li>
+            <ShoppingCartIcon
+              className={
+                numberCartItems == 0
+                  ? "text-gray-600 w-10 h-10"
+                  : "text-white w-10 h-10"
+              }
+            />
+          </li>
+          {numberCartItems > 0 && (
+            <span className="absolute top-0 right-0 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-xs font-bold text-white">
+              {numberCartItems}
+            </span>
+          )}
         </Link>
-        
       </ul>
     </nav>
   );
