f779634e429b0788573b9aa88b74df40e833c5b4
Ulfath <ulfathwaliullah@gmail.com>
2026-04-13 11:50:10 +0200

    Add logic to only add product to cart if it is not in the cart otherwise increase the quantity.

--- components/AddToCartButton.tsx
+++ components/AddToCartButton.tsx
@@ -4,10 +4,22 @@ import { useCartStore } from "./Store";
 
 export default function AddToCartButton({ product }: { product: Product }) {
   const addProductToCart = useCartStore((state) => state.addProduct);
+  const incrementQuantity = useCartStore((state) => state.incrementQuantity);
+  const cartItems = useCartStore((state) => state.cartItems);
+  function handleOnClick(cartItem: CartItem) {
+    const hasProduct = cartItems.some(
+      (item) => item.product.id === cartItem.product.id,
+    );
+    if (hasProduct) {
+      incrementQuantity(cartItem);
+    } else {
+      addProductToCart(cartItem);
+    }
+  }
   const cartItem: CartItem = { product: product, quantity: 1 };
   return (
     <button
-      onClick={() => addProductToCart(cartItem)}
+      onClick={() => handleOnClick(cartItem)}
       type="button"
       className="px-6 py-3 w-full rounded-full bg-[#3338ff] text-white mt-6 hover:bg-[#1e21ff] transition-colors"
     >
