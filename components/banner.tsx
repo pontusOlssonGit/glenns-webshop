72048e8594ad7b78e40dc3a45842864c4cbc7015
pontusOlssonGit <pontusOlssonEmail@gmail.com>
2026-04-14 21:38:09 +0200

    fixes-nav-z-index

--- components/banner.tsx
+++ components/banner.tsx
@@ -10,18 +10,20 @@ export default function Banner ({products}: {products?: Product[]}) {
         <h1 className="text-md font-semibold">Fri frakt över 500 000 kr.</h1>
         <p className="text-sm text-gray-700 pb-4">Din destination för de bästa produkterna.</p>
 
-        <div className='flex flex-col lg:flex-row bg-white p-6'>
+        <div className='flex flex-col xl:flex-row gap-2 bg-white p-6'>
+        <div>
         <Image 
             src={"/assets/banner.png"}
             alt="Banner Image"
             width={800}
             height={400}
-            className="w-full object-cover rounded-md"
+            className="w-full object-cover rounded-md h-full"
         />
-        <div className='hidden xl:flex xl:flex-col gap-4 lg:pl-4 justify-center items-start'>
+        </div>
+        <div className='hidden xl:flex xl:flex-col gap-4 xl:pl-4 justify-center items-start'>
           <h2 className='text-lg font-semibold'>Upptäck våra senaste produkter!</h2>
           {products && products.map((product) => (
-            <div key={product.id} className='flex flex-row items-center justify-center w-full gap-4 p-2 border-b border-gray-200'>
+            <div key={product.id} className='flex flex-row items-center justify-start w-full gap-4 p-2 border-b border-gray-200'>
               <Image
                 src={product.thumbnail}
                 alt={product.title}
