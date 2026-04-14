4c78666eaebc70843bc43f9a1f0795be0fa8354b
md1frejo <jonas.mb@gmail.com>
2026-04-15 00:13:06 +0200

    pdf increased, removed duplicate in code

--- product-page-client.tsx
+++ product-page-client.tsx
@@ -24,7 +24,6 @@ export default function ProductsPageClient({ products }: { products: Product[] }
 
       if (!error && data) {
         setFilteredProducts(data);
-        setFilteredProducts(data);
       }
     };
 
