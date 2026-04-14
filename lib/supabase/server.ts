6e8c38ca35af546943515f5793ddd116ffca7efc
md1frejo <jonas.mb@gmail.com>
2026-04-14 14:21:59 +0200

    server.ts has no statements in catch

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -21,6 +21,7 @@ export async function createClient() {
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
+	    // nohting here, remove?
           }
         },
       },
