f30cc857ce1faa021fad6b6c916c11bd5bb1ff07
Ulfath <ulfathwaliullah@gmail.com>
2026-04-14 13:08:34 +0200

    Fix styling in the cart

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -9,24 +9,31 @@ export default function Cart() {
     return acc + item.product.price * item.quantity;
   }, 0);
   return (
-    <div className="pt-4 px-5 bg-white">
-      <h1 className="text-2xl mx-2 font-bold border-b-2 pb-4">Varukorg</h1>
-      {productsInCart.length > 0 ? (
-        productsInCart.map((cartItem: CartItem) => (
-          <CartCard key={cartItem.product.id} product={cartItem.product} />
-        ))
-      ) : (
-        <article>
-          <span className="text-center py-10 text-gray-500 ">
-            Your cart is empty.
-          </span>
-        </article>
-      )}
+    <div className="pt-4 px-5 bg-white flex flex-col gap-6 ">
+      <h1 className="text-2xl mx-2 font-bold border-b-2 pb-4 border-black">
+        Varukorg
+      </h1>
+
+      <div className="divide-y divide-[#d6d6d6]">
+        {productsInCart.length > 0 ? (
+          productsInCart.map((cartItem: CartItem) => (
+            <div key={cartItem.product.id}>
+              <CartCard key={cartItem.product.id} product={cartItem.product} />
+            </div>
+          ))
+        ) : (
+          <article>
+            <span className="text-center py-10 text-gray-500 justify-center flex">
+              Din varukorg är tom
+            </span>
+          </article>
+        )}
+      </div>
 
       {productsInCart.length > 0 && (
-        <div className=" p-4 px-80 bg-gray-50 flex justify-between items-center ">
-          <span className="font-bold">Summa</span>
-          <span>{Math.ceil(totalPrice)}</span>
+        <div className=" p-2  flex justify-between items-center bg-[#f6f6f6]">
+          <span className="font-bold">Summa:</span>
+          <span>{Math.ceil(totalPrice)} kr</span>
         </div>
       )}
     </div>
