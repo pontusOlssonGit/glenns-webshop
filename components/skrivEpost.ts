0ae445c0b89e3113ab582bb77795526a5363dc4c
IlluminatorWatch <adam@pluxemburg.com>
2026-04-14 17:12:03 +0200

    first commit

--- components/skrivEpost.ts
+++ components/skrivEpost.ts
@@ -3,22 +3,31 @@
 import nodemailer from "nodemailer";
 
 export async function skrivEpost(data: FormData) {
-    const formNamn = data.get("name") as string;
-    const formEmail = data.get("email") as string;
-    const formSubject = data.get("subject") as string;
-    const formMessage = data.get("message") as string;
-    
-    const epostMessage=  "<b>Från:</b> " + formNamn + " | " + "<b>Epost:</b> " + formEmail + + "<br> <b>Gällande:</b> " + formSubject + "<br>" + formMessage;
+  const formNamn = data.get("name") as string;
+  const formEmail = data.get("email") as string;
+  const formSubject = data.get("subject") as string;
+  const formMessage = data.get("message") as string;
 
-    const transporter = nodemailer.createTransport({
-    host:  process.env.SMTP_HOST,
+  const epostMessage =
+    "<b>Från:</b> " +
+    formNamn +
+    " | " +
+    "<b>Epost:</b> " +
+    formEmail +
+    +"<br> <b>Gällande:</b> " +
+    formSubject +
+    "<br>" +
+    formMessage;
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
     secure: false, // TLS via STARTTLS
     auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
     },
-}); 
+  });
 
   const info = await transporter.sendMail({
     from: process.env.SMTP_USER,
@@ -27,6 +36,5 @@ export async function skrivEpost(data: FormData) {
     html: epostMessage,
   });
 
-  console.log('Message sent: ', info.messageId);
-
+  console.log("Message sent: ", info.messageId);
 }
